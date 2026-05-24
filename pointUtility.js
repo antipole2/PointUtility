4259ee4f27acac62d9905c19b5d7fb9220c62010
antipole2 <github@antipole.co.uk>
2026-05-24 13:14:42 +0100

    Squashed commit of the following:
    commit 80edc62363e5ef67b4eec049ae59a079ab7b70cb
    Author: antipole2 <github@antipole.co.uk>
    Date:   Sun May 24 13:11:11 2026 +0100
    
        Update to v3.3
    
    commit 89b9a868a11e9e03282445e3d466e859e5d7515f
    Author: Tony <github@antipole.co.uk>
    Date:   Sun May 24 12:59:37 2026 +0100
    
        Revise README for v3.3
    
        Updated README to clarify functionality and installation requirements.
    
    commit d4b3f782e08f8a203d4b30efdc17b4384e2c24c7
    Author: antipole2 <github@antipole.co.uk>
    Date:   Sun May 24 12:42:07 2026 +0100
    
        Patch version & date in JSON file
    
    commit 88dfe4ef0f9a40ebb83ba790f903d532fb02729c
    Author: antipole2 <github@antipole.co.uk>
    Date:   Sun May 24 12:15:41 2026 +0100
    
        Revert required plugin version to 4.0.0
    
    commit a0b88d0905d2efb5526cdb98264e3cd6e1c40de2
    Merge: 0e99447 115961b
    Author: antipole2 <github@antipole.co.uk>
    Date:   Sat Apr 4 17:45:05 2026 +0100
    
        Merge branch 'v3.2' of https://github.com/antipole2/PointUtility into v3.2
    
    commit 0e99447e165ee47cc4153a6421ab296854527a1f
    Author: antipole2 <github@antipole.co.uk>
    Date:   Sat Apr 4 17:44:33 2026 +0100
    
        Improve check for required plugin version
    
    commit 115961b48a9fedec7d742f23f39f9dc6caca7100
    Author: Tony <github@antipole.co.uk>
    Date:   Sat Apr 4 16:51:13 2026 +0100
    
        Update version, release date and add duplicate route
    
    commit 00a71e73185c4e5fec5374ae8d8680f7159a618d
    Author: Tony <github@antipole.co.uk>
    Date:   Sat Apr 4 15:42:37 2026 +0100
    
        Update context menu descriptions and version requirement
    
    commit 334ccc7e6bc80e9de7fb26d0aeefb8c6a2e08628
    Author: antipole2 <github@antipole.co.uk>
    Date:   Sat Apr 4 15:30:47 2026 +0100
    
        Update to 3.2

--- pointUtility.js
+++ pointUtility.js
@@ -1,24 +1,26 @@
 scriptName = "PointUtility";
-near = 0.25;	// copied waypoint must be this near nm
-
-confirmDrops = false;
-dumpOnParseFail = true;
-
-require("pluginVersion")("3.1.1");
-scriptName = "PointUtility";
-scriptVersion = "1.5.1" // Uses built-in updating + fix for missing trace declaration
-require("checkForUpdate")(scriptName, scriptVersion, 5, "https://raw.githubusercontent.com/antipole2/PointUtility/main/version.JSON");
 
 var trace = false;
 if (!trace) consolePark();
 
-Position = require("Position");
+confirmDrops = false;
+dumpOnParseFail = true;
+
+scriptName = "PointUtility";
+thisScriptVersion = "3.3";
+require("checkForUpdate")(scriptName, thisScriptVersion, 5, "https://raw.githubusercontent.com/antipole2/PointUtility/v3.2/version.JSON");
+
+// Because of an error in the Position contstuctor in JS plugin v4.0.0,
+// we need to use a patched version appended below and not the one in the plugin.
+// So next line commented out
+// Position = require("Position");
+
+pasteMarkId = false;		// if we have pasteMark menu,id of its callback
 
 //first time set up _remember if need be
 if ((typeof _remember == "undefined")
 	|| (_remember == null)
-	|| (!_remember.hasOwnProperty("pointUtilitySet"))){
-	// first time
+	|| (!_remember.hasOwnProperty("pointUtilitySet"))){ // first time
 	consoleName(scriptName);
 	_remember = {};
 	_remember.pointUtilitySet = false;
@@ -26,44 +28,35 @@ if ((typeof _remember == "undefined")
 	}
 if (trace) print(_remember, "\n");
 
-OCPNonContextMenu(copyPos,"Copy position");
-OCPNonContextMenu(copyMark, "Copy mark");
+OCPNonAllContextMenu(copyPos,"Copy position");
+OCPNonAllWaypointContextMenu(copyMark, "Copy mark");
+OCPNonAllRouteContextMenu(dupRoute, "Duplicate route");
 
 if (_remember.pointUtilitySet){
-	OCPNonContextMenu(pasteMark, "Paste mark");
-	OCPNonContextMenu(handleClipboard, "Paste mark from clipboard");
+	pasteMarkId = OCPNonAllContextMenu(pasteMark, "Paste mark");
+	OCPNonAllContextMenu(handleClipboard, "Paste mark from clipboard");
 	}
 
 function copyPos(pos){	// copy formatted position to clipboard
+	if (trace) print("Cppy position: ", pos, "\n");
 	toClipboard(new Position(pos).formatted);
-	OCPNonContextMenu(copyPos,"Copy position");
 	}
 
-function copyMark(location){	// copy nearest mark as pro forma
+function copyMark(info){	// copy mark as pro forma
 	if (trace) print("In copyMark\n");
-	nearby = findNearby(location);
-
-	if (nearby == ""){
-		report(" No single nearest waypoint within " + near + "nm");
-		OCPNonContextMenu(copyMark, "Copy mark");
-		return;
+	theMark = OCPNgetSingleWaypoint(info.GUID);
+	if (trace) print("Copied mark ", theMark.markName, "\n");
+	_remember.mark = theMark;
+	_remember.pointUtilitySet = true;
+	if (!pasteMarkId){	// did not have paste mark available before, so add pasting options
+		pasteMarkId = OCPNonAllContextMenu(pasteMark, "Paste mark");
+		OCPNonAllContextMenu(handleClipboard, "Paste mark from clipboard");
 		}
-	else {
-		_remember.mark = nearby;
-		_remember.pointUtilitySet = true;
-		}
-	if (trace) print("Pro forma mark copied\n_remember now: ", _remember, "\n");
-	OCPNonContextMenu();	// not sure if we had one, so start again
-	OCPNonContextMenu(copyPos,"Copy position");
-	OCPNonContextMenu(copyMark, "Copy mark");
-	OCPNonContextMenu(pasteMark, "Paste mark");
-	OCPNonContextMenu(handleClipboard, "Paste mark from clipboard");
 	}
 	
 function pasteMark(pos){	// paste pro forma mark at pos after confirming
 	name = "pasted"+_remember.suffix++;
 	confirm(name, pos);
-	OCPNonContextMenu(pasteMark, "Paste mark");
 	}
 
 function confirm(name, position){	// give user chance to confirm mark
@@ -124,8 +117,7 @@ function handleClipboard(){
 	parts = text.match(partPat);	// into parts
 	if (trace) print("Parts: ", parts, "\n");
 	if (parts == null){
-		report("No valid position pair on clipboard");
-		OCPNonContextMenu(handleClipboard, "Paste mark from clipboard");
+		report("No recognised position pair on clipboard");
 		return;
 		}
 	else {
@@ -146,37 +138,137 @@ function handleClipboard(){
 		if (trace) print("From clipboard name '", namePart, "'\tPosition: ", position, "\n");
 		confirm(namePart, position);
 		}
-	OCPNonContextMenu(handleClipboard, "Paste mark from clipboard");
 	}
-	
-function findNearby(pos){
-	// find waypoint near pos
-	waypoints = [];	// array of candidates
-	guids = OCPNgetWaypointGUIDs();
-	if (guids.length < 1) return "";
-	for (g = 0; g < guids.length; g++){
-		waypoint = OCPNgetSingleWaypoint(guids[g]);
-		if (!waypoint.isVisible) continue;
-		waypoint.distance = OCPNgetVectorPP(pos, waypoint.position).distance;
-		if (waypoint.distance > near) continue;
-		waypoints.push(waypoint);	// remmember candidate
-		}
-	if (waypoints.length < 1) return "";
-	else if (waypoints.length == 1) return waypoints[0];	// only one
-	waypoints.sort(function(a, b){return a.distance - b.distance});
-	if (!waypoints[1].distance > waypoints[0].distance){
-		report("Mark selection ambiguous - more than one nearest");
-		return "";
-		}
-	return waypoints[0];
+
+function dupRoute(input){ // duplicate selected route
+	if (trace) print("In dupRoute\n");
+	route = OCPNgetRoute(input.GUID);
+	existingName = route.name;
+	if (trace) print("Selected route is ", route.name, "\n");
+	route.isVisible = false;
+	OCPNupdateRoute(route);	// hide original copy
+	route.isVisible = true;
+	route.name += " copy";
+	delete route.GUID;	// get a new route created
+	OCPNaddRoute(route);
+	report("Route '" + existingName + "' has been hidden\nRoute '" + route.name + "' created");
 	}
 
 function report(message){
 	onSeconds();	// cancel any existing timer
 	alert(message);
-	onSeconds(cancel, 7);
+	onSeconds(function(){alert(false);}, 10);	// cancel alert after time
 	}
 
-function cancel(){
-	alert(false);
-	}
+/***************************************************************
+ * Patched version of Position contstructor
+ ***************************************************************/
+ function Position(lat, lon){
+	if (arguments.length == 1){
+		if (typeof arguments[0] != "string"){
+			this.latitude = arguments[0].latitude;
+			this.longitude = arguments[0].longitude;
+			}
+		else {3
+			pos = parsePosition(arguments[0]);
+			this.latitude = pos.latitude;
+			this.longitude = pos.longitude;
+			}
+		}
+	else {
+		this.latitude= lat;
+		this.longitude = lon;
+		}
+	
+	Object.defineProperty(this, "formatted",{  // format for human eye
+			enumerable: false,
+			configurable: false,
+			get: function () {
+				return(niceify(OCPNformatDMS(1, this.latitude,true)) + " " + niceify(OCPNformatDMS(2, this.longitude,true)));
+				}
+			});
+	
+	Object.defineProperty(this, "NMEA",{   // format as in NMEA sentence
+			enumerable: false,
+			configurable: false,
+			get: function () {
+				latAbs = Math.abs(this.latitude);  wholeLatAbs =  Math.floor(latAbs);
+				lonAbs = Math.abs(this.longitude);  wholeLonAbs =  Math.floor(lonAbs);
+				return (
+					((wholeLatAbs*100) + ((latAbs%1)*60)).toFixed(5) + "," + ((this.latitude < 0) ? "S," : "N,") +
+					("00" + (((wholeLonAbs*100) + ((lonAbs%1)*60)).toFixed(5))).slice(-11) + "," + ((this.longitude < 0) ? "W" : "E")
+					)
+				}
+			});
+	
+	this.NMEAdecode = function(string, n) {  // decodes n'th position from NMEA string
+		n -= 1;  // base 0
+		decoded = string.match(/\d+\.\d+,[NS],\d+\.\d+,[EW]/g);
+		if (!decoded) return(false);
+		poses = decoded[n].match(/\d+\.\d+/g);
+		if (poses.length != 2) return false;
+		whole = parseInt(poses[0]/100);  // Latitude
+		rest = poses[0] - whole*100;
+		this.latitude = whole + rest/60;
+		whole = parseInt(poses[1]/100);  // Longitude
+		rest = poses[1] - whole*100;
+		this.longitude = whole + rest/60;
+		poses = decoded[n].match(/[NSEW]/g);  // hemispheres
+		if (poses[0] == "S") this.latitude *= -1;
+		if (poses[1] == "W") this.longitude *= -1;
+		return(true);
+		}
+	
+	this.latest = function(){
+		fix = OCPNgetNavigation();
+		this.latitude = fix.position.latitude;
+		this.longitude = fix.position.longitude;
+		this.time = fix.fixTime;
+		}
+	
+	this.parse = function(text){
+		return parsePosition(text);
+		}
+	
+	return this;
+	
+	function parsePosition(text){
+		// parse lat & long into position object
+		trace = false;
+		title = arguments.callee.name;	// remember for error reporting
+		phase = "";	// for error reporting
+		partPat = /(.*)(N|S)(.*)(E|W)/i;
+		parts = text.match(partPat);	// into parts
+		if (parts == null) bail("not lat long pair");	
+		if (trace) print("Halves: ", parts, "\n");
+		if (parts.length != 5) bail("parse error 1");
+		position = {};
+		position.latitude = OCPNparseDMS(parts[1]+parts[2]);
+		position.longitude = OCPNparseDMS(parts[3]+parts[4]);
+		return position;
+		function bail(message){
+			throw(title + " " + phase + " " + message);
+			};
+		}
+	
+	function niceify(string){	// ajust precision to my liking
+		parts = string.split(" ");
+		switch (parts.length){
+			case 1:	// Decimal degrees - do nothing
+			return(string);
+			case 2:	// should not be
+			return("Nonsense formatting of decimal degrees");
+			case 3:	// degrees and decimal minutes
+			value = parts[1].slice(0,-1)* 1;
+			parts[1] = value.toFixed(3) + "'";			
+			return parts.join(" ");
+			case 4:	// degrees minutes and decimal seconds
+			value = parts[2].slice(0,-1)* 1;
+			parts[2] = value.toFixed(1) + "\"";			
+			return parts.join(" ");
+			default: break;
+			}			
+		return("Invalid formatted string from OCPN");;
+		}
+	
+	};
